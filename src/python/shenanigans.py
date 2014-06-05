3cb8d78fcab2c16ea6929cf8b1a615de0903c038
root <root@localhost.localdomain>
2014-06-05 19:28:49 -0400

    Working packet sniffer.

--- src/python/shenanigans.py
+++ src/python/shenanigans.py
@@ -1,25 +1,72 @@
-#!/opt/local/bin/python2.7
+#/opt/local/bin/python2.7
 
 from scapy.all import *
+import sqlite3
 
-class Shenanigans:
+
+class ProbePacket:
+
+	def __init__(self, packet):
+		self.packet = packet;
+
+# Sniffs all apparently valid wifi probes, using Scapy
+class ProbeSniffer:
 	def __init__(self):
 		pass
 
+	def makePacket(x):
+
+	def getPacketDetails(self, x):
+		layer = x.getlayer(Dot11ProbeReq)
+		layer2 = layer.getlayer(Dot11Elt)
+		mac = x.getlayer(Dot11).getfieldval("addr2")
+		ssid = layer2[0].getfieldval("info")
+		return (mac,ssid)
+
+	
+	def probefilter (x):
+			#return True;
+			if not x.haslayer(Dot11ProbeReq) return False
+			
+			return ssid > "" and all(c in string.printable for c in ssid)
+			#if ssid > "" and all(c in string.printable for c in ssid):
+				#print ("\t".join([mac,ssid]))
+			#	return True
+			#else:
+			#	return False
+
+	def probefound (x):
+		packet = ProbePacket
+		found.probe()
+		
+	def start(self, found, iface="mon0"):
+		self.found = found
+		sniff(iface=iface, lfilter=probefilter, prn=probefound)
+
+	
+class SQLPacketStore:
+	def __init__(self):
+		self.con = sqlite3.connect(":memory:")
+		self.con.isolation_level = None
+		
+
+	def initTables():
+
+	def refreshTable():
+
+
+
+class StorageStrategy:
+	def store(x):
+
+
+class Shenanigans:
+	def __init__(self):
+
 	def start(self):
-		def probefilter (x):
-			return True;
-			#return x.haslayer(Dot11ProbeReq)
 
-		def probefound (x):
-			print x.summary()
-
-		sniff(iface="en0", lfilter=probefilter, prn=probefound)
-
-	
-
-	
 
 
 if __name__ == '__main__':
   Shenanigans().start();
+  
